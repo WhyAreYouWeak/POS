b906dfc9df84756fc8537987f08e02ceb3e1ab33
DANIEL\Daniel <danielzarodnansky@gmail.com>
2024-01-08 16:03:12 +0100

    mem leaks

--- client/main.cpp
+++ client/main.cpp
@@ -2,17 +2,24 @@
 #include "sockets/my_socket.h"
 #include "SocketReceive.h"
 #include "PongGame.h"
-#include <mutex>
+#include <crtdbg.h>
 #include "Objects.h"
 
 int main() {
-    MySocket* mySocket = MySocket::createConnection("frios2.fri.uniza.sk", 12456);
-    MessageBuffer messageBuffer;
-    TempStruct tempStruct;
-    std::thread readThread(ReadSocket, mySocket->connectSocket, std::ref(messageBuffer), std::ref(tempStruct));
-    PongGame pongGame(800, 450, "Pong Game", mySocket, tempStruct);
-    pongGame.run();
-    mySocket->sendEndMessage();
-    delete mySocket;
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    {
+        MySocket *mySocket = MySocket::createConnection("frios2.fri.uniza.sk", 19195);
+        MessageBuffer messageBuffer;
+        TempStruct tempStruct;
+        std::thread readThread(ReadSocket, mySocket->connectSocket, std::ref(messageBuffer), std::ref(tempStruct));
+
+        PongGame pongGame(800, 450, "Pong Game", mySocket, tempStruct);
+        pongGame.run();
+        mySocket->sendEndMessage();
+        delete mySocket;
+    }
+
     return 0;
 }
